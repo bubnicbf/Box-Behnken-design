97121594c2c51e73d03fe3f9b308d4387c63f959
Ben Bubnick <ben.bubnick@gmail.com>
2015-05-12 11:56:40 -0400

    Create r8mat_transpose_print_some.m

---
+++ r8mat_transpose_print_some.m
@@ -0,0 +1,45 @@
+/*
+prints some of an R8MAT, transposed
+*/
+
+function r8mat_transpose_print_some ( m, n, a, ilo, jlo, ihi, jhi, title )
+
+  incx = 5;
+
+  fprintf ( 1, '\n' );
+  fprintf ( 1, '%s\n', title );
+
+  for i2lo = max ( ilo, 1 ) : incx : min ( ihi, m )
+
+    i2hi = i2lo + incx - 1;
+    i2hi = min ( i2hi, m );
+    i2hi = min ( i2hi, ihi );
+
+    inc = i2hi + 1 - i2lo;
+    
+    fprintf ( 1, '\n' );
+    fprintf ( 1, '  Row: ' );
+    for i = i2lo : i2hi
+      fprintf ( 1, '%7d       ', i );
+    end
+    fprintf ( 1, '\n' );
+    fprintf ( 1, '  Col\n' );
+
+    j2lo = max ( jlo, 1 );
+    j2hi = min ( jhi, n );
+
+    for j = j2lo : j2hi
+
+      fprintf ( 1, '%5d ', j );
+      for i2 = 1 : inc
+        i = i2lo - 1 + i2;
+        fprintf ( 1, '%12f', a(i,j) );
+      end
+      fprintf ( 1, '\n' );
+
+    end
+
+  end
+
+  return
+end
